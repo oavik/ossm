355b0ec7224e95cd8054d959a348980e6eb1d216
Oavik <oavik@oavik.net>
2015-06-17 21:51:14 -0400

    add insulting ossm.sh to repo so the file exists at all

---
+++ ossm.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+printf 'Content-Type: text/plain\r\n\r\n'
+printf 'Yes, this is going to be a CMS written in POSIX sh.\n'
+printf 'Why? Because fuck you, that'\''s why.\n'
+printf 'Also, fuck Python. And Perl. And ESPECIALLY PHP.\n'
